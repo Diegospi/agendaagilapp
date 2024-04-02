7aa0bc64784840caba553da30306d4a9998ce62b
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-04-02 16:46:08 -0500

    se actualiza formulario y se crea Rutas

--- src/Formulario/Formulario.jsx
+++ src/Formulario/Formulario.jsx
@@ -1,11 +1,23 @@
 import { useState } from "react"
+import { useNavigate } from "react-router-dom"
 
 export function Formulario() {
 
-    const[verCedula,modificarCedula]=useState("")
-    const[verContraseña,modificarContraseña]=useState("")
+    const[verCedula,guardarCedula]=useState("")
+    const[verContraseña,guardarContraseña]=useState("")
 
-    function procesarFormulario(){}
+    //para navegar entre componentes debo declarar una variable que almacene el hook
+    let enrutador=useNavigate()
+
+
+    function procesarFormulario(evento){
+        //Que hago si le hacen clic al boton del formulario?
+        evento.preventDefault()
+        console.log("Le hicieron clic al boton del formulario")
+
+        //voy a enrutar otro componente (¿Como lanzo un componente desde otro?)
+        enrutador("/home")
+    }
 
     return (
         <>
@@ -13,7 +25,7 @@ export function Formulario() {
                 <div className="row justify-content-center text-center">
                     <div className="col-12 col-md-6">
                         <img src="../../src/assets/logo.png" alt="" className="img-fluid" />
-                        <form className="border rounded p-4">
+                        <form className="border rounded p-4" onSubmit={procesarFormulario}>
                             <h2>Controla tu salud</h2>
 
                             <div class="input-group mb-3 mt-4">
@@ -23,6 +35,7 @@ export function Formulario() {
                                 class="form-control" 
                                 placeholder="Numero cedula"
                                 id="identificacion"
+                                onChange={function(evento){guardarCedula(evento.target.value)}}
                                 />
                             </div>
                             <div class="input-group mb-3 mt-4">
@@ -32,6 +45,7 @@ export function Formulario() {
                                 class="form-control" 
                                 placeholder="Contraseña"
                                 id="contraseña"
+                                onChange={function(evento){guardarContraseña(evento.target.value)}}
                                 />
                             </div>
 
